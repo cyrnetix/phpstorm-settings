a7c86436a0eaa2bc9366f53595ac3f2462e71465
r.ennik <r.ennik@daisycon.com>
2017-03-16 11:40:32 +0100

    PS-2016.3.2 <r.ennik@Daisy-131 Create patriot.icls, m_js_controller.js, sourlick.icls, peacock.icls, PHP_3.jpg, Default copy.xml, Anja_0.icls, Laracasts_0.icls, shrek.icls, find.xml, expansion_publ.js, html_0.jpg, keymap.xml, rainbow.icls, Dreamweaver.icls, push(2).html, snappy.icls, peacocks-in-space.icls, l, super.icls, laravel.icls, Default.xml, screen-01_2.jpg, freshcut.icls, php_2.jpg, scheme-php_0.jpg, goldfish.icls, slate.icls, keen.icls, revelation.icls, tribal.icls, potpourri.icls, editor.xml, npp-1_1.jpg, piggy.icls, debugger.xml, baseRefactoring.xml, ide.general.xml, git.xml, zacks.icls, saved_resource(2).html, darkside.icls, juicy.icls, yule.icls, solarflare.icls, ui.lnf.xml, zrt_lookup.html, _ramon_dark.icls, heroku.icls, drupal.js, ads(2).html, l(1), tonic.icls, saved_resource(3).html, jquery.once.js, ads(1).html, frontier.icls, 728x90.html, stark.icls, bootstrap(1).js, arstotzka.icls, carbonight.icls, External Tools.xml, iceberg.icls, JzpTCSqH5bLUPxqs74yxMZjBNspSW8NTHpaYBQK07U4.js, show_ads.js, Default _1_.xml, web-browsers.xml, peel.icls, Themes _ PhpStorm Themes & Color Styles.html, tron.icls, l(2), legacy.icls, IntelliLang.xml, saved_resource(4).html, Monokai copy - Kopie_0.icl, project.default.xml, diff.xml, ads.html, vagrant.xml, custom.js, 10543747249118245516, googleanalytics.js, spearmint.icls, application-octet-stream.png, fleure2_1.jpeg, code.style.schemes.xml, halflife.icls, Darcula copy.icls, bold.icls, 1_0.JPG, Notepad__.icls__2.icls, earthsong.icls, slime.icls, saved_resource(1).html, gloom.icls, glowfish.icls, adsbygoogle.js, Herokoon.icls, push(1).html, otakon.icls, crisp.icls, php_1.jpg, css, abg.js, grunge.icls, saved_resource.html, soft-color_0.icls, jquery.min.js, googlelogo_color_112x36dp.png, 2015-03-13 14-41-53 Скриншот экрана_2.jpg, addata.js, osd.js, other.xml, saved_resource(6).html, logo.png, ca-pub-0619816986353921.js, _ramon_light.icls, push.html, php.xml, bootstrap.js, colors.scheme.xml, analytics.js, tweenmax_1.18.0_499ba64a23378545748ff12d372e59e9_min.js, lavender.icls, chocolate.icls, azure.icls, boxuk.icls, mellow.icls, Norway Today by Erik Kubica_0.icl, kiwi.icls, turnip.icls, github.icls, better_exposed_filters.js, laf.xml, Dark _ Blue_0.icls, pastel.icls, saved_resource(5).html, show_ads_impl.js, bootstrap-fileupload.min.js, hyrule.icls, mintchoc.icls, vcs.xml, databaseSettings.xml, usageView.xml, bootstrap.css, mud.icls, exitapi-impl.js

---
+++ colors/Themes _ PhpStorm Themes & Color Styles_files/custom.js
@@ -0,0 +1,12 @@
+(function ($) {
+
+  Drupal.behaviors.phpstorm = {
+    attach: function (context) {
+
+      $('#edit-title-wrapper #edit-title').attr("placeholder", "Search");
+
+
+    }
+  };
+
+})(jQuery);
